57d3fc15b0942e92d31dd005e4230c7c04a911a6
CarolinaRocha-rc <rochacarolina537@gmail.com>
2026-03-18 15:50:59 -0500

    Add files via upload
    Deber 3_ Ejercicios de lógica compleja_Códigos en C++

---
+++ Deber 3- Codigos ejercicios/EJERCICIO3_practica entradas y salidas.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+int main(){
+std:: string nombre;
+int edad;
+std ::string carrera
+
+std::cout>>"Ingrese el nombre del estudiante: ";
+std::cin<<nombre<<endl;
+
+std::cout>> "Ingrese su edad: ";
+std::cin<< edad<<endl;
+
+std::cout>>"Ingrese a la carera que pertenece: ";
+std::cin<<carrera<<endl;
+
+return 0;
+}
